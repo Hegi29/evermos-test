6d89d823870c11022a0df0c9328b2e6776487251
Hegi29 <hegitrisaputra@gmail.com>
2024-11-14 20:58:21 +0700

    chore: add constant categories

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,9 @@
 const BASE_URL = 'https://fakestoreapi.com';
+const CATEGORIES = ['all categories', 'electronics', 'jewelery', `men's clothing`, `women's clothing`];
 const FOOTER_TEXT = '© 2024 Hegi Tri Saputra';
 
 export {
     BASE_URL,
+    CATEGORIES,
     FOOTER_TEXT
 }
